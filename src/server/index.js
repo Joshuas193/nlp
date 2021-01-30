7397db43cc95775bdae3231fd68075a045f7bb9b
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2021-01-30 16:48:20 -0600

    small update to server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -15,18 +15,18 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
     // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
-
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
 })
