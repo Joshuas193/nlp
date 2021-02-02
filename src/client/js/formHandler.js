e110c50da29af9e8b9d6a8b2bea34f3fc2a1dd92
Joshua Swartzell <joshuas193@live.com>
2021-02-02 14:37:22 -0600

    Updating JS files to incorporate API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,13 +1,14 @@
-const baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
+const options = "&lang=en&URL=";
+const apiKey = `${process.env.API_KEY}`;
 
-function handleSubmit(event) {
+/* function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test')
     .then(res => {
         return res.json()
@@ -15,6 +16,18 @@ function handleSubmit(event) {
     .then(function(data) {
         document.getElementById('results').innerHTML = data.message
     })
+} */
+
+const fetchData = async (baseUrl, options, apiKey, chosenUrl) => {
+    let chosenUrl = document.getElementById('chosenUrl');
+    const res = await fetch(baseUrl+apiKey+options+chosenUrl)
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }   catch(error)    {
+        console.log('error', error);
+    }
 }
 
 export { handleSubmit }
