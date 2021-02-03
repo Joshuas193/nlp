a74a493023502280dd3da5fd2394b2814d1d1676
Joshua Swartzell <joshuas193@live.com>
2021-02-03 14:31:40 -0600

    Refactoring many things. Totally broken

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,7 +2,17 @@ function handleSubmit(event) {
     event.preventDefault()
     // check what text was put into the form field
     const chosenUrl = document.getElementById('chosenUrl').value
-    console.log(chosenUrl);
+    console.log(chosenUrl)
+    postData(chosenUrl.value)
+}
+
+const postData = async (url) => {
+    const response = await fetch('/api', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(url)
+    })
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
+projectData = {};
+
 require('dotenv').config();
 var path = require('path')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
+const fetch = require('node-fetch'); //added to make fetch requests from the server side
 
 app.use(cors());
 // to use json
@@ -15,20 +18,30 @@ app.use(bodyParser.urlencoded({
 // using the generated Dist folder rather than the larger files
 app.use(express.static('dist'));
 
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+  console.log('Example app listening on port 8081!')
+})
+
+/*GET request using ENV variable for API key
 app.get('/api/:chosenUrl', async (req, res) => {
   const chosenUrl = req.params.chosenUrl;
+  console.log(chosenUrl);
   const baseUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}`;
   const options = `&lang=auto&URL=${chosenUrl}`;
   const fetch_data = await fetch(baseUrl+options);
+  console.log(fetch_data);
   const data = await fetch_data.json();
   res.json(data);
-})
+  console.log(data);
+}) */
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
-})
+app.post('/api', (req, res) => {
+  console.log(req);
+  projectData = req.body;
+  res.send(projectData);
+});
