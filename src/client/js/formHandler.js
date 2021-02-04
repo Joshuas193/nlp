e7a6859ce7daac05fb651bb77b64b023eb79a929
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2021-02-03 23:37:02 -0600

    reverted many things back

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,17 +1,15 @@
 function handleSubmit(event) {
     event.preventDefault()
-    // check what text was put into the form field
-    const chosenUrl = document.getElementById('chosenUrl').value
-    console.log(chosenUrl)
-    postData(chosenUrl.value)
-}
 
-const postData = async (url) => {
-    const response = await fetch('/api', {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(url)
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
+    checkForName(formText)
+
+    console.log("::: Form Submitted :::")
+    fetch('http://localhost:8080/test')
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = res.message
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,10 @@
-projectData = {};
-
 require('dotenv').config();
 var path = require('path')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
-const fetch = require('node-fetch'); //added to make fetch requests from the server side
+const fetch = require('node-fetch'); //added to make fetch requests from the server
 
 app.use(cors());
 // to use json
@@ -23,25 +21,6 @@ app.listen(8081, function () {
   console.log('Example app listening on port 8081!')
 })
 
-/*GET request using ENV variable for API key
-app.get('/api/:chosenUrl', async (req, res) => {
-  const chosenUrl = req.params.chosenUrl;
-  console.log(chosenUrl);
-  const baseUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}`;
-  const options = `&lang=auto&URL=${chosenUrl}`;
-  const fetch_data = await fetch(baseUrl+options);
-  console.log(fetch_data);
-  const data = await fetch_data.json();
-  res.json(data);
-  console.log(data);
-}) */
-
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
-
-app.post('/api', (req, res) => {
-  console.log(req);
-  projectData = req.body;
-  res.send(projectData);
-});
