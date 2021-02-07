f196e6ad2e1755cc1bb4a89630b643ad3f2b9cc8
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2021-02-07 16:46:49 -0600

    A couple visual updates

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,30 +1,29 @@
 const handleSubmit = async (event) => {
-    event.preventDefault()
-    // check what text was put into the form field
-    const userInput = document.getElementById('userInput').value;
-    console.log(userInput);
-    postData('http://localhost:8081/apiData', { userInput: userInput })
-}
+  event.preventDefault();
+  // check what text was put into the form field
+  const userInput = document.getElementById("userInput").value;
+  console.log(userInput);
+  postData("http://localhost:8081/apiData", { userInput: userInput });
+};
 
-const postData = async ( url = '', data = {}) => {
-    const response = await fetch(url, {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  try {
+    const data = await response.json().then((data) => {
+      document.getElementById("score_tag").innerHTML = `Score Tag: ${data.score_tag}`;
+      document.getElementById("agreement").innerHTML = `Agreement: ${data.agreement}`;
+      document.getElementById("subjectivity").innerHTML = `Subjectivity: ${data.subjectivity}`;
+      document.getElementById("confidence").innerHTML = `Confidence: ${data.confidence}`;
+      document.getElementById("irony").innerHTML = `Irony: ${data.irony}`;
     });
-    try {
-      const data = await response.json()
-      .then(data => {
-          document.getElementById('score_tag').innerHTML = `Score Tag: ${data.score_tag}`;
-          document.getElementById('agreement').innerHTML = `Agreement: ${data.agreement}`;
-          document.getElementById('subjectivity').innerHTML = `Subjectivity: ${data.subjectivity}`;
-          document.getElementById('confidence').innerHTML = `Confidence: ${data.confidence}`;
-          document.getElementById('irony').innerHTML = `Irony: ${data.irony}`;
-      });
-    } catch(error) {
-      console.log('error', error);
-    }
-}
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-export { handleSubmit }
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,48 +1,47 @@
-require('dotenv').config();
-var path = require('path')
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
-const fetch = require('node-fetch'); //added to make fetch requests from the server
-
-
-projectData = {};
+const fetch = require("node-fetch"); //added to make fetch requests from the server
 
 app.use(cors());
 // to use json
 app.use(bodyParser.json());
 // to use url encoded values
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 // using the generated Dist folder rather than the larger files
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-  console.log('Example app listening on port 8081!')
-})
+  console.log("Example app listening on port 8081!");
+});
 
-app.get('/', function (req, res) {
-  res.sendFile('dist/index.html')
-})
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
 
-app.post('/apiData', async (req, res) => {
+//Making API call and sending back selected data to the client
+app.post("/apiData", async (req, res) => {
   const userInput = req.body.userInput;
   const baseUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}`;
   const options = `&lang=en&model=general&url=${userInput}`;
-  const apiUrl = (baseUrl+options);
+  const apiUrl = baseUrl + options;
   const fetchRes = await fetch(apiUrl);
   const json = await fetchRes.json()
-  //console.log(json)
-  .then((data) => {
-    res.send({
-      score_tag: data.score_tag,
-      agreement: data.agreement,
-      subjectivity: data.subjectivity,
-      confidence: data.confidence,
-      irony: data.irony
+    //console.log(json)
+    .then((data) => {
+      res.send({
+        score_tag: data.score_tag,
+        agreement: data.agreement,
+        subjectivity: data.subjectivity,
+        confidence: data.confidence,
+        irony: data.irony,
+      });
     });
-  });
 });
