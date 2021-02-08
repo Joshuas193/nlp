1511ef64f5cc855b791281044d564e4ab5c105d1
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2021-02-08 16:10:59 -0600

    Starting to add testing

---
+++ src/client/__tests__/formHandler.test.js
@@ -0,0 +1,8 @@
+const postData = require('..js/formHandler');
+
+test('the function queries a url and sends back data', async => {
+  const url = 'https://www.washingtonpost.com/us-policy/2021/02/08/democrats-stimulus-checks-covid-relief/';
+  const data = await postData('http://localhost:8081/apiData', { url })
+  .then()
+
+})
